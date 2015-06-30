aabfe327fd839deb0be263a05618b1de5b901cfb
Ivan Novosad <ivan.novosad@gmail.com>
2015-06-30 10:00:42 +0200

    Add more links to images

--- lib/lita/handlers/tomorrow.rb
+++ lib/lita/handlers/tomorrow.rb
@@ -11,7 +11,9 @@ module Lita
         "http://www.quickmeme.com/img/11/11fc9fa5e8a7c96992a955f1d7dcf7392b327d158eadfdcf9b3f31ba493dbd90.jpg",
         "http://ct.fra.bz/ol/fz/sw/i50/5/8/12/frabz-So-you-mean-to-tell-me-i-dont-have-to-go-to-work-tomorrow-02f8f1.jpg",
         "http://www.quickmeme.com/img/c0/c00f12b1e0c0ec2a2990e4992564be6cd424b1e0f33359ca8d77391fe358f12a.jpg",
-        "http://img.pandawhale.com/post-19767-tomorrow-is-a-good-day-meme-Au-TSNs.jpeg"
+        "http://img.pandawhale.com/post-19767-tomorrow-is-a-good-day-meme-Au-TSNs.jpeg",
+        "https://schistoryday.files.wordpress.com/2014/02/tomorrow.jpg",
+        "https://peonut.files.wordpress.com/2013/05/20130522-214604.jpg"
       ]
 
       def self.default_config(config)
